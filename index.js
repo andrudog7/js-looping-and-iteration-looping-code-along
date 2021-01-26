0fa51da25adcf49cdd6f32daedc68cf4c90d66a6
--add <andrewksmoker@gmail.com>
2021-01-26 10:01:09 -0500

    commit tree da50d8e90dae2395847c1cb34b7d68951647fbf6

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, holiday) {
+    let thankYouCards = []
+    for (let i =0; i < names.length; i++) {
+        thankYouCards.push(`Thank you, ${names[i]}, for the wonderful ${holiday} gift!`)
+    }
+    return thankYouCards
+}
+
+function countDown(integer) {
+    let i = integer 
+    while (i >= 0) {
+        console.log(i);
+        i -= 1;
+    }
+}
